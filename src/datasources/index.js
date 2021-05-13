bc7a269349fbaa2c6a3a536822fae232e46ebaf8
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-05-12 22:59:30 -0500

    [JB] extract datasources

---
+++ src/datasources/index.js
@@ -0,0 +1,40 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class NASAAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://api.nasa.gov'
+    }
+
+    async getApod() {
+        return await this.get(
+            `planetary/apod?api_key=${process.env.NASA}`);
+    }
+
+    async getNEOs() {
+        return await this.get(
+            `neo/rest/v1/feed/today?detailed=false&api_key=${process.env.NASA}`
+        );
+    }
+}
+
+class ISSAPI extends RESTDataSource {
+    constructor() {
+        // Always call super()
+        super();
+        // Sets the base URL for the REST API
+        this.baseURL = 'https://api.wheretheiss.at/v1/satellites';
+    }
+
+    async getSatellites() {
+        // Send a GET request to the specified endpoint
+        return this.get(`/`);
+    }
+
+    async getLocations() {
+        return this.get('/25544/positions?timestamps=1436029892,1436029902&units=miles')
+    }
+}
+
+exports.NASAAPI = NASAAPI;
+exports.ISSAPI = ISSAPI;
