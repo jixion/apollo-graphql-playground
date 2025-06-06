9a9ad032d62f426b64d66b563629e7acbe444254
Jeff Burroughs <jburroughs@sparkimaginations.com>
2025-06-06 02:33:00 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const { NASAAPI, ISSAPI, COTDBAPI, OWMAPI } = require('./datasources');
 function authenticateAppEngineService(req, res, next) {
     console.log(req.headers.host);
     console.log(req.header["host"]);
-    if (req.header["host"].endsWith('.aqueous-cargo-415820.uc.r.appspot.com')) {
+    if (req.headers.host.endsWith('.aqueous-cargo-415820.uc.r.appspot.com')) {
         next(); // Allow the request
     } else {
         res.status(403).send('Forbidden: Access denied.');
